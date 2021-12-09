2d09627c22bf4639ca03e60b6bda6bdbed798192
hainguyen <haigom00@gmail.com>
2021-12-09 16:40:31 +0700

    connect db

--- JavaServer/src/main/java/JavaServer/JavaServerApplication.java
+++ JavaServer/src/main/java/JavaServer/JavaServerApplication.java
@@ -3,11 +3,35 @@ package JavaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 @SpringBootApplication
 public class JavaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaServerApplication.class, args);
-	}
+		
 
+		ConnectionString connectionString = new ConnectionString("mongodb+srv://ptud:ptud@cluster0.7gw7q.mongodb.net/PTUD?retryWrites=true&w=majority");
+		MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+		MongoClient mongoClient = MongoClients.create(settings);
+		MongoDatabase database = mongoClient.getDatabase("PTUD");
+		
+		try {
+			System.out.println(database.listCollectionNames());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
