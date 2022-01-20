f750ebf77a05815413e91e15da4dc4ff79b14cfc
hainguyen <haigom00@gmail.com>
2022-01-20 21:36:22 +0700

    return salary per day

--- ServerJavaTest/src/main/java/ServerJavaTest/controller/ShipperController.java
+++ ServerJavaTest/src/main/java/ServerJavaTest/controller/ShipperController.java
@@ -8,8 +8,10 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.CollectionUtils;
+import org.springframework.data.mongodb.core.geo.GeoJson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,6 +19,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import ServerJavaTest.model.Order;
 import ServerJavaTest.model.Shipper;
@@ -41,7 +47,7 @@ public class ShipperController {
 	
 	@GetMapping("/shipper/{shipperId}/salary/month/{month}/year/{year}")
 	@CrossOrigin(origins = "http://localhost:8080")
-	public ResponseEntity<Integer> getSalary(@PathVariable("shipperId") String shipperId, @PathVariable("month") Integer month, @PathVariable("year") Integer year)
+	public ResponseEntity<JsonObject> getSalary(@PathVariable("shipperId") String shipperId, @PathVariable("month") Integer month, @PathVariable("year") Integer year)
 	{
 		Optional<Shipper> shipper = repo.findById(shipperId);
 		if(!shipper.isPresent())
@@ -61,12 +67,42 @@ public class ShipperController {
 			{
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 			}else {
-				Integer salary = 0;
+				List<Integer> day = new ArrayList<Integer>();
+				;
 				for(int i = 0; i < listOrder.size(); i++) {
-					salary += listOrder.get(i).getShipper_fee();
+					if(!day.contains((listOrder.get(i)).getCreated_at().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfMonth()))
+					{
+						day.add((listOrder.get(i)).getCreated_at().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfMonth());
+					}
 				}
-				
-				return new ResponseEntity<>(salary, HttpStatus.OK);
+				String salary = "{";
+				List<Order> newListOrder = new ArrayList<Order>();
+				Integer count;
+				for(int i = 0; i < day.size(); i++)
+				{
+					
+					Integer currentDay = day.get(i);
+					count = 0;
+
+					newListOrder.addAll(listOrder);
+					CollectionUtils.filter(newListOrder, order -> Objects.equals( ((Order) order).getCreated_at().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfMonth() , currentDay));
+					
+					for(int j = 0; j < newListOrder.size(); j++)
+					{
+						
+						count += newListOrder.get(j).getShipper_fee();
+					}
+					if(i == day.size() - 1 )
+					{
+						salary += "\n" + day.get(i).toString() + " : " + count.toString();
+					}else {
+
+						salary += "\n" + day.get(i).toString() + " : " + count.toString() + ",";
+					}
+				}
+				salary += "}";
+				JsonObject jsonObject = new Gson().fromJson(salary, JsonObject.class);
+				return new ResponseEntity<>(jsonObject, HttpStatus.OK);
 			}
 		}
 	}
