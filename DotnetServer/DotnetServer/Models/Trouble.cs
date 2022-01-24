efc2198248c881b419b66fc864b22509eb52a83d
alanda2013 <71961256+alanda2013@users.noreply.github.com>
2022-01-24 20:06:50 +0700

    API Trouble ExtenInfor

---
+++ DotnetServer/DotnetServer/Models/Trouble.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace DotnetServer.Models
+{
+    public class Trouble
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string order_id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string shipper_id { get; set; }
+     
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+        public string description { get; set; }
+
+        public int status { get; set; }
+
+        public string picture { get; set; }
+    }
+}
---
+++ DotnetServer/DotnetServer/Startup.cs
@@ -0,0 +1,88 @@
+using DotnetServer.Models;
+using DotnetServer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string MyAllowSpecificOrigins = "";
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader()
+                                  .AllowAnyMethod();
+                });
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotnetServer", Version = "v1" });
+            });
+            services.Configure<DatabaseSettings>(
+        Configuration.GetSection(nameof(DatabaseSettings)));
+
+            services.AddSingleton<IDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+            services.AddSingleton<ShipperService>();
+            services.AddSingleton<OrderService>();
+            services.AddSingleton<ShopService>();
+            services.AddSingleton<CustomerService>();
+            services.AddSingleton<UserService>();
+            services.AddSingleton<TroubleService>();
+            services.AddSingleton<ExtenInforService>();
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            string MyAllowSpecificOrigins = "";
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DotnetServer v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
